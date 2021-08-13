ae6dbcc2f3d3bfcb8fc10e230174faaaa0c9a31e
liam <ZhangHY1012@gmail.com>
2021-08-13 20:38:13 +0900

    change folder name

---
+++ graphics_model/build/CMakeFiles/GM.dir/DependInfo.cmake
@@ -0,0 +1,53 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/kismet/Desktop/qt/stock/build/GM_autogen/mocs_compilation.cpp" "/Users/kismet/Desktop/qt/stock/build/CMakeFiles/GM.dir/GM_autogen/mocs_compilation.cpp.o"
+  "/Users/kismet/Desktop/qt/stock/build/qrc_resource.cpp" "/Users/kismet/Desktop/qt/stock/build/CMakeFiles/GM.dir/qrc_resource.cpp.o"
+  "/Users/kismet/Desktop/qt/stock/src/gmedge.cpp" "/Users/kismet/Desktop/qt/stock/build/CMakeFiles/GM.dir/src/gmedge.cpp.o"
+  "/Users/kismet/Desktop/qt/stock/src/gmgraphicsview.cpp" "/Users/kismet/Desktop/qt/stock/build/CMakeFiles/GM.dir/src/gmgraphicsview.cpp.o"
+  "/Users/kismet/Desktop/qt/stock/src/gmnode.cpp" "/Users/kismet/Desktop/qt/stock/build/CMakeFiles/GM.dir/src/gmnode.cpp.o"
+  "/Users/kismet/Desktop/qt/stock/src/gmqtgraphicscuttingline.cpp" "/Users/kismet/Desktop/qt/stock/build/CMakeFiles/GM.dir/src/gmqtgraphicscuttingline.cpp.o"
+  "/Users/kismet/Desktop/qt/stock/src/gmqtgraphicsedge.cpp" "/Users/kismet/Desktop/qt/stock/build/CMakeFiles/GM.dir/src/gmqtgraphicsedge.cpp.o"
+  "/Users/kismet/Desktop/qt/stock/src/gmqtgraphicsnode.cpp" "/Users/kismet/Desktop/qt/stock/build/CMakeFiles/GM.dir/src/gmqtgraphicsnode.cpp.o"
+  "/Users/kismet/Desktop/qt/stock/src/gmqtgraphicsscene.cpp" "/Users/kismet/Desktop/qt/stock/build/CMakeFiles/GM.dir/src/gmqtgraphicsscene.cpp.o"
+  "/Users/kismet/Desktop/qt/stock/src/gmqtgraphicssocket.cpp" "/Users/kismet/Desktop/qt/stock/build/CMakeFiles/GM.dir/src/gmqtgraphicssocket.cpp.o"
+  "/Users/kismet/Desktop/qt/stock/src/gmscene.cpp" "/Users/kismet/Desktop/qt/stock/build/CMakeFiles/GM.dir/src/gmscene.cpp.o"
+  "/Users/kismet/Desktop/qt/stock/src/gmsocket.cpp" "/Users/kismet/Desktop/qt/stock/build/CMakeFiles/GM.dir/src/gmsocket.cpp.o"
+  "/Users/kismet/Desktop/qt/stock/src/main.cpp" "/Users/kismet/Desktop/qt/stock/build/CMakeFiles/GM.dir/src/main.cpp.o"
+  "/Users/kismet/Desktop/qt/stock/src/mainwindow.cpp" "/Users/kismet/Desktop/qt/stock/build/CMakeFiles/GM.dir/src/mainwindow.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../"
+  "GM_autogen/include"
+  ".././src"
+  ".././include"
+  "/usr/local/Cellar/qt@5/5.15.2/lib/QtWidgets.framework"
+  "/usr/local/Cellar/qt@5/5.15.2/lib/QtWidgets.framework/Headers"
+  "/usr/local/Cellar/qt@5/5.15.2/lib/QtGui.framework"
+  "/usr/local/Cellar/qt@5/5.15.2/lib/QtGui.framework/Headers"
+  "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.15.sdk/System/Library/Frameworks/OpenGL.framework/Headers"
+  "/usr/local/Cellar/qt@5/5.15.2/lib/QtCore.framework"
+  "/usr/local/Cellar/qt@5/5.15.2/lib/QtCore.framework/Headers"
+  "/usr/local/Cellar/qt@5/5.15.2/./mkspecs/macx-clang"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ graphics_model/build/CMakeFiles/GM.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GM_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/GM_autogen.dir/ParseCache.txt"
+  "GM_autogen"
+  "CMakeFiles/GM.dir/GM_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/GM.dir/qrc_resource.cpp.o"
+  "CMakeFiles/GM.dir/src/gmedge.cpp.o"
+  "CMakeFiles/GM.dir/src/gmgraphicsview.cpp.o"
+  "CMakeFiles/GM.dir/src/gmnode.cpp.o"
+  "CMakeFiles/GM.dir/src/gmqtgraphicscuttingline.cpp.o"
+  "CMakeFiles/GM.dir/src/gmqtgraphicsedge.cpp.o"
+  "CMakeFiles/GM.dir/src/gmqtgraphicsnode.cpp.o"
+  "CMakeFiles/GM.dir/src/gmqtgraphicsscene.cpp.o"
+  "CMakeFiles/GM.dir/src/gmqtgraphicssocket.cpp.o"
+  "CMakeFiles/GM.dir/src/gmscene.cpp.o"
+  "CMakeFiles/GM.dir/src/gmsocket.cpp.o"
+  "CMakeFiles/GM.dir/src/main.cpp.o"
+  "CMakeFiles/GM.dir/src/mainwindow.cpp.o"
+  "GM"
+  "GM.pdb"
+  "qrc_resource.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
